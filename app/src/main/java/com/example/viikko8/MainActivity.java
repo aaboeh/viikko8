ecf92485a2f6fc85cc198fdc54038e2d8b2c3903
aaboeh <aapo.aarvala@gmail.com>
2025-02-28 17:19:24 +0200

    Viikko 8 tehtävät

---
+++ app/src/main/java/com/example/viikko8/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.viikko8;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText EuroInput;
+    private TextView ResultText;
+    private Button USDButton;
+    private Button GDBButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EuroInput = findViewById(R.id.EuroInput);
+        ResultText = findViewById(R.id.ResultText);
+        USDButton = findViewById(R.id.USDButton);
+        GDBButton = findViewById(R.id.GDBButton);
+    }
+
+    public void convertToDollars(View view) {
+        String inputText = EuroInput.getText().toString();
+
+        double euros = Double.parseDouble(inputText);
+        double dollars = euros * 1.05;
+        String result = String.format("%s%.2f", dollars);
+        ResultText.setText(result);
+    }
+
+    public void convertToPounds(View view) {
+        String inputText = EuroInput.getText().toString();
+
+        double euros = Double.parseDouble(inputText);
+        double pounds = euros * 0.83;
+        String result = String.format("%s%.2f", pounds);
+        ResultText.setText(result);
+    }
+}
